d9af50557d5723f0355a196c5e785796818f8f58
andreydara <andreydara@gmail.com>
2025-04-18 18:03:58 +0200

    test: simple ndvi test

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -3,6 +3,7 @@ from ingestion.custom_indices import ndvi
 
 
 def test_ndvi_simple():
+    """Test NDVI calculation with simple arrays."""
     red = np.array([0.2, 0.5, 0.0])
     nir = np.array([0.8, 0.5, 1.0])
     expected = (nir - red) / (nir + red)
