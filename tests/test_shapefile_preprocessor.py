cfd1e773d6898e2b610fc08186cf4b287cc4ee8a
andreydara <andreydara@gmail.com>
2025-04-18 21:04:11 +0200

    Revert "refactor: unused variable"
    This reverts commit b921f25acc941b3de73f5b12653542b465949fb0.

--- tests/test_shapefile_preprocessor.py
+++ tests/test_shapefile_preprocessor.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 from ingestion.shapefile_preprocessor import ShapefilePreprocessor
 
 
-def test_processor_creates_output(tmp_path):
+def test_processor_creates_output(tmp_path, sample_shapefile):
     # copy or generate a tiny shapefile into tmp_path
     processor = ShapefilePreprocessor(str(tmp_path))
     processor.run()
